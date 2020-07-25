ba47a53afaea2c7a03bf37dddab6e1a004a430c1
Daniel Aibar <mrdbar@gmail.com>
2020-07-24 22:24:15 -0700

    first commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Mary Sue" },
+                new Customer { Id = 2, Name = "John Doe"}
+            };
+
+            var viewModel = new CustomersViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+
+
+        public ActionResult Details(int id)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Mary Sue" },
+                new Customer { Id = 2, Name = "John Doe"}
+            };
+
+            var customer = customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek!" },
+                new Movie { Id = 2, Name = "Cinderella"}
+            };
+
+            var viewModel = new MoviesViewModel
+            {
+                Movies = movies
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek!" },
+                new Movie { Id = 2, Name = "Cinderella"}
+            };
+
+            var movie = movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+    }
+}
